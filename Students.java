c756122bc793107300754b91e73b056aa2c6c419
Viran Pravinda <h.viran.pravinda.10@student.scu.edu.au>
2022-04-09 19:52:40 +0800

    readStudentsFile method created

--- Students.java
+++ Students.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 /**
  * This class reads unit name and students' info including last name,
@@ -9,7 +12,10 @@
  */
 public class Students
 {
-    // instance variables - replace the example below with your own
+    // instance variables
+    private String line;
+    private String unitName;
+    private String[] tokens;
 
     /**
      * Constructor for objects of class Students
@@ -17,5 +23,39 @@ public class Students
     public Students()
     {
         // initialise instance variables
+    }
+
+    
+    /**
+     * Method readStudentsFile
+     * This method reads data from the input file
+     * @param
+     * @return
+     */
+    public void readStudentsFile() {
+        int lineCount = 0;
+        try {
+            File studentsFile = new File("prog5001_students_grade_2022.csv");
+            Scanner myScanner = new Scanner(studentsFile);
+            
+            while(myScanner.hasNextLine()) {
+                lineCount++;
+                line = myScanner.nextLine();
+                
+                if(lineCount == 1) {
+                    unitName = line;
+                    System.out.println(unitName);
+                }
+                
+            }
+        } catch(FileNotFoundException ex) {
+            System.out.println("This file cannot be found");
+            ex.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        Students obj = new Students();
+        obj.readStudentsFile();
     }
 }
