98536f900f3bbb9ea3492a08ad66f51e2282e3d4
Viran Pravinda <h.viran.pravinda.10@student.scu.edu.au>
2022-04-09 19:20:44 +0800

    Initial commit

---
+++ Students.java
@@ -0,0 +1,21 @@
+
+/**
+ * This class reads unit name and students' info including last name,
+ * first name, student id, assignment1 marks, assignment2 marks, assignment 3 marks 
+ * and calculates the total marks of each student. Then prints the results.
+ *
+ * @author (Viran Pravinda)
+ * @version (09-04-2022)
+ */
+public class Students
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class Students
+     */
+    public Students()
+    {
+        // initialise instance variables
+    }
+}
